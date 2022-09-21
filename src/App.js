8db44b33ac777d85e83fa33fb12209a98decd32f
Bhavya0404 <singhpappu966@gmail.com>
2022-09-21 23:51:01 +0530

    created algo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,75 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import { ListCoins } from "./urls";
+import { useEffect, useState } from "react";
 
-const getUsers = async () => {
-  const { data } = await axios.get(ListCoins("AAPL"));
-  console.log(data.close);
-};
 
 function App() {
+  const [low, setLow] = useState(0);
+  const [high, setHigh] = useState(0);
+  const [close, setClose] = useState(0);
+  const [values, setValues] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {data} = await axios.get(ListCoins("AAPL"));
+      setValues(data);
+      setLow(parseFloat(data.low));
+      setHigh(parseFloat(data.high));
+      setClose(parseFloat(data.close));
+      console.log(data)
+    };
+    fetchData()
+  }, [])
+
+  // const GetUsers = async () => {
+    
+    // let high = parseFloat(values.high);
+    // let low = parseFloat(values.low);
+    // let close = parseFloat(values.close);
+    const woodiePivot = () =>{
+      let pp = (high + low + close) / 3;
+      let r2 = pp + high - low;
+      let r1 = 2 * pp - low;
+      let s1 = 2 * pp - high;
+      let s2 = pp - high + low;
+  
+      console.log(s2);
+    }
+
+    const camarillaPivot = () =>{
+      let r4 = close + ((high - low)* 1.5000)
+      let r3 = close + ((high - low)* 1.2500)
+      let r2 = close + ((high - low)* 1.1666)
+      let r1 = close + ((high - low)* 1.0833)
+      let pp = (high + low + close) / 3;
+      let s1 = close - ((high - low)* 1.0833)
+      let s2 = close - ((high - low)* 1.1666)
+      let s3 = close - ((high - low)* 1.2500)
+      let s4 = close - ((high - low)* 1.5000)
+  
+      console.log(s2);
+    }
+
+    const fibonacciPivot = ()=> {
+      let pp = (high + low + close) / 3;
+      let r3 = pp + ((high - low)* 1.000)
+      let r2 = pp + ((high - low)* .618)
+      let r1 = pp + ((high - low)* .382)
+
+      let s1 = pp - ((high - low)* .382)
+      let s2 = pp - ((high - low)* .618)
+      let s3 = pp - ((high - low)* 1.000)
+      console.log(s2);
+
+    }
+    
+
   return (
     <div className="App">
-      <button onClick={getUsers}>click</button>
+      <button onClick={woodiePivot}>click</button>
+      <button onClick={camarillaPivot}>click</button>
+      <button onClick={fibonacciPivot}>click</button>
     </div>
   );
 }
