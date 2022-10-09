459ab29b4df7529d00081097d141b1497d306b47
Meenal Prakash <meenalprakash03@gmail.com>
2022-10-09 15:36:48 +0530

    apiconnected

--- src/components/ListStock.js
+++ src/components/ListStock.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from "react";
 import axios from "axios";
 import { ListCoins, RealTime } from "../urls";
 import { useEffect, useState } from "react";
 import { TimeWise } from "../urls";
 const ListStock = () => {
-    useEffect(() => {
-        const fetchDateandTime = async () => {
-          const  data1  = await axios.get(TimeWise("IBM"));
-          console.log(data1);
-        };
-        fetchDateandTime();
-      }, []);
-  return (
-    <div>ListStock</div>
-  )
-}
+  useEffect(() => {
+    const fetchDateandTime = async () => {
+      const data1 = await axios.get(TimeWise("IBM"));
 
-export default ListStock
+      const neww = data1.data["Time Series (5min)"];
+      let sampleObjectKeys = Object.keys(neww);
+      console.log("number of keys", sampleObjectKeys.length);
+      for(var key in neww) {
+        
+            console.log(neww[key]["1. open"])
+        
+     }
+
+      console.log(data1.data["Time Series (5min)"]);
+    };
+    fetchDateandTime();
+  }, []);
+  return <div>ListStock</div>;
+};
+
+export default ListStock;
